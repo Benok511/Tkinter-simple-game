ac0e48ca9774611837d7ac8619f2cc1983525c86
benok511 <benokeeffe76@gmail.com>
2025-11-19 16:42:16 +0000

    used .after instead of sleep to stop freezes

--- game.py
+++ game.py
@@ -5,11 +5,13 @@ from time import sleep
 from oval import Oval
 
 '''
-https://www.w3schools.com/python/python_variables_global.asp
 https://tkinter-docs.readthedocs.io/en/latest/widgets/canvas.html
 
-had to look up how to make variable global as I didnt know!
-also had to read into the tkinter canvas docs to find out how to clear it
+had to read into the tkinter canvas docs to find out how to clear it to find out about tags and how to use them to delete stuff
+
+after quite a lot of buggy iterations and some research and you bringing it up in the lecture I decided to use .after instead
+of sleep as it didnt freeze my program and make it sluggish.
+https://stackoverflow.com/questions/10393886/tkinter-and-time-sleep
 '''
 
 
@@ -31,15 +33,16 @@ def clickOval(event):
         canvas.delete('oval')
         oval.onScreen = False
 
-    else:
-        drawOval()
+    
+    root.after(randint(1000,5000),drawOval)
     
     
 
 def drawOval():
-    sleep(randint(1,5))
-    oval.new_oval()
-    canvas.create_oval(oval.x0,oval.y0,oval.x1,oval.y1,fill='blue',tags='oval') #setting a tag so its easy to delete with canvas.delete('oval')
+    
+    if not oval.onScreen:
+        oval.new_oval()
+        canvas.create_oval(oval.x0,oval.y0,oval.x1,oval.y1,fill='blue',tags='oval') #setting a tag so its easy to delete with canvas.delete('oval')
     
     
 
@@ -48,6 +51,7 @@ canvas = Canvas(root,bg='white',height=300,width=300)
 canvas.pack()
 
 canvas.bind("<Button-1>",clickOval)
+drawOval()
 
 
 root.mainloop()
